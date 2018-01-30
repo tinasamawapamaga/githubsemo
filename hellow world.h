52565262bac4bb8773ebf69a7a42a92c6c86d2bf
tinasamawapamaga <tlv144199@gmail.com>
2018-01-29 18:50:00 -0600

    Update hellow world.h
    Function returns the sum.

--- hellow world.h
+++ hellow world.h
@@ -1,8 +1,15 @@
 #include <iostream>
+
+int add(int x; int y); 
+
 int main()
 {
 	int a=0;
 	std::cout<<"Hellow World!\n";
 	std::cout<<"a="<< a<<endl;
 	return 0;
+}
+int add(int x, int y)	
+{
+	return x+y;
 }
